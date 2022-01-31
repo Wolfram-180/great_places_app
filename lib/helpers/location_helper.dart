d8c3d7b2abbb488c6343740f739ca0b4914f2119
Wolfram-180 <sergei.ulvis@gmail.com>
2022-01-31 16:23:54 +0300

    working on address definition

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import '../keys/google_api_key.dart' as api_key;
 
 final GOOGLE_API_KEY = api_key.GOOGLE_API_KEY;
@@ -6,5 +10,12 @@ class LocationHelper {
   static String generateLocationPreviewImage(
       {required double? latitude, required double? longitude}) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
+    final response = await http.get(Uri.parse(url));
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
